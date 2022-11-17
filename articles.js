d7c316585ede19c0cd446e6444170679f7109c82
David Daza <03dazal@gmail.com>
2022-11-16 21:46:41 -0500

    Polish articles scraper

--- articles.js
+++ articles.js
@@ -1,58 +1,95 @@
+const axios = require('axios')
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 const path = require('path')
+const { format, parseISO } = require('date-fns')
 
-const file = path.join(__dirname, 'news.json');
+const file = path.join(__dirname, 'data', 'articles.json')
 
-(
-  async () => {
-    const url = 'https://www.semana.com/buscador/?query=didier%20tavera'
-    const browser = await puppeteer.launch(/* { headless: false } */)
-    const page = await browser.newPage()
-    await page.goto(url)
-    let news = []
-    let noticias = await page.$$('div.queryly_item_row')
-    let id = 0
-    let btnNext = await page.$('a.next_btn')
+const endpoint = 'https://api.queryly.com/json.aspx?queryly_key=06e63be824464567&query=didier%20tavera%20&endindex={{PAGE}}&batchsize=20&callback=searchPage.resultcallback&showfaceted=true&timezoneoffset=300'
 
-    while (btnNext) {
-      btnNext = await page.$('a.next_btn')
-      if (btnNext) {
-        noticias = await page.$$('div.queryly_item_row')
-        for (let i = 0; i < noticias.length; i++) {
-          const data = await noticias[i].evaluate((element) => {
-            const news = element
-            return {
-              tittle: news.querySelector('.queryly_item_title').textContent || '',
-              date: news.querySelector('a div').textContent || '',
-              url: news.querySelector('a').href || '',
-              description: news.querySelector('.queryly_item_description').textContent || ''
-            }
-          })
-          const { url } = data
-          const pageArticle = await browser.newPage()
-          await pageArticle.goto(url)
-          const article = await pageArticle.$('div.section.sp-12')
-          const content = await article.evaluate(() => {
-            const header = document.querySelector('.article-header-box')
-            const article = document.querySelector('.paywall').innerHTML
-            return {
-              abstract: header.querySelector('h2').textContent || '',
-              date: header.querySelector('.datetime').textContent || '',
-              article
+async function getURLs () {
+  const results = []
 
-            }
-          })
-          console.log(content)
-          news = [...news, { id, ...data, ...content }]
-          await pageArticle.close()
+  let collect = true
+  let index = 0
+  let page = 20 * index
 
-          id++
-        }
-        await page.waitForTimeout(2000)
-        await page.click('a.next_btn')
-      }
+  while (collect) {
+    const response = await axios.get(endpoint.replace('{{PAGE}}', page))
+    const responseText = response.data.trim()
+    const re = /"items":(\[.*?\])/
+    const match = responseText.match(re)
+    if (!match) {
+      break
     }
-    fs.writeFileSync(file, JSON.stringify(news), 'utf8')
-    browser.close()
-  })()
+    const [, itemsText] = match
+    const items = JSON.parse(itemsText || [])
+    if (!items.length) {
+      collect = false
+    } else {
+      const links = items.map(item => item.link)
+      results.push(...links)
+
+      index += 1
+      page = 20 * index
+    }
+  }
+  return results
+}
+
+(async () => {
+  const urls = await getURLs()
+  const browser = await puppeteer.launch(/* { headless: false } */)
+  const page = await browser.newPage()
+
+  const results = []
+  let missing = 0
+  let warnings = 0
+  const total = urls.length
+
+  for (let index = 0; index < total; index++) {
+    console.log(`Processing ${index + 1} / ${total}`)
+    const url = urls[index]
+    const response = await page.goto('https://www.semana.com' + url, {
+      timeout: 60000
+    })
+    if (response.status() === 404) {
+      missing += 1
+      console.log('Not found X')
+      console.log('\n')
+      continue
+    }
+    const data = await page.evaluate(() => {
+      const overlineText = document.querySelector('h3.overlineText ')?.textContent ?? ''
+      const title = document.querySelector(overlineText === 'opinión' ? '.article-box-opinion h2' : '.article-main h1')
+      const excerpt = document.querySelector('h2.h2')?.textContent ?? ''
+      const publishedDate = document.querySelector('.datetime')?.textContent ?? ''
+      const [day, month, year] = publishedDate.split('/')
+      const content = Array.from(document.querySelectorAll('article.paywall p.section.sp-8'))
+      return {
+        title: title?.textContent ?? '',
+        excerpt,
+        publishedDate: publishedDate ? new Date(+year, +month - 1, +day).toISOString() : '',
+        content: content.length ? content.reduce((result, p) => result + `<p>${p.innerHTML}</p>`, '') : '',
+        scope: overlineText
+      }
+    })
+    if (!data.title) {
+      console.log('I could\'t find the title 🤔')
+      warnings += 1
+    }
+    if (data.publishedDate) {
+      data.publishedDate = format(parseISO(data.publishedDate), 'yyyy-MM-dd')
+    }
+    results.push(data)
+    console.log('Processed ✓')
+    console.log('\n')
+  }
+  await browser.close()
+  const success = results.filter(item => item.title)
+  fs.writeFileSync(file, JSON.stringify(success), 'utf8')
+  console.log(`✓ ${results.length - missing - warnings}`)
+  console.log(`⚠ ${warnings} entries omitted because title is missing`)
+  console.log(`X ${missing} missing pages`)
+})()
